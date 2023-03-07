a6a27756a184baccddaa7a1ad7f30beac923425a
Benj-Qu <quzhemin@umich.com>
2023-03-07 16:38:52 -0500

    PacketHeader

--- WTP-base/utils/PacketHeader.cpp
+++ WTP-base/utils/PacketHeader.cpp
@@ -6,17 +6,17 @@
 #define INT_SIZE 4
 
 PacketHeader::PacketHeader(char* buffer) {
-    memcpy((void*)(&(this->type)), (void*)(buffer + 0 * INT_SIZE), INT_SIZE);
-	memcpy((void*)(&(this->seqNum)), (void*)(buffer + 1 * INT_SIZE), INT_SIZE);
-	memcpy((void*)(&(this->length)), (void*)(buffer + 2 * INT_SIZE), INT_SIZE);
-	memcpy((void*)(&(this->checksum)), (void*)(buffer + 3 * INT_SIZE), INT_SIZE);
+    memcpy(&(this->type), buffer + 0 * INT_SIZE, INT_SIZE);
+	memcpy(&(this->seqNum), buffer + 1 * INT_SIZE, INT_SIZE);
+	memcpy(&(this->length), buffer + 2 * INT_SIZE, INT_SIZE);
+	memcpy(&(this->checksum), buffer + 3 * INT_SIZE, INT_SIZE);
 }
 
 void PacketHeader::encode(char* buffer) {
-    memcpy((void*)(buffer + 0 * INT_SIZE), (void*)(&(this->type)), INT_SIZE);
-	memcpy((void*)(buffer + 1 * INT_SIZE), (void*)(&(this->seqNum)), INT_SIZE);
-	memcpy((void*)(buffer + 2 * INT_SIZE), (void*)(&(this->length)), INT_SIZE);
-	memcpy((void*)(buffer + 3 * INT_SIZE), (void*)(&(this->checksum)), INT_SIZE);
+    memcpy(buffer + 0 * INT_SIZE, &(this->type), INT_SIZE);
+	memcpy(buffer + 1 * INT_SIZE, &(this->seqNum), INT_SIZE);
+	memcpy(buffer + 2 * INT_SIZE, &(this->length), INT_SIZE);
+	memcpy(buffer + 3 * INT_SIZE, &(this->checksum), INT_SIZE);
 }
 
 void PacketHeader::log(std::ofstream& ofs) {
