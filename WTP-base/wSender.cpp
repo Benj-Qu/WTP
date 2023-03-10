282b66e20c22119e5c83a3b4fb62a74e37cab4ba
Benj-Qu <quzhemin@umich.com>
2023-03-10 00:27:06 -0500

    wSender

--- WTP-base/wSender.cpp
+++ WTP-base/wSender.cpp
@@ -1,34 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 
 #include "utils/Info.hpp"
-#include "utils/Clock.hpp"
 #include "utils/Packet.hpp"
 #include "utils/PacketHeader.hpp"
 #include "utils/Window.hpp"
 #include "utils/udp.hpp"
 
-std::mutex mtx;
-std::mutex lk;
-std::condition_variable cv;
-
-Window window;
-Clock timer;
-bool alive = true;
-
-void CheckTime(AddrInfo* sender, std::ofstream& log);
-void CheckAck(AddrInfo* recver, std::ofstream& log);
-void SendSignal(Packet packet, AddrInfo* sender, std::ofstream& log);
-
-/*  Thread 0: 
- *  Build Connection. Send START Packet. Send File As Packets. Send END Packet.
- *  Push Packet To Window When Sent.
- *  Blocked by CV When Window Full.
- */
 int main(int argc, char **argv) {
     SenderInfo info(argc, argv);
 
@@ -39,13 +18,11 @@ int main(int argc, char **argv) {
         std::cerr << "Failed Creating Socket" << std::endl;
         exit(1);
     }
-
     // Set Timeout
     struct timeval tv;
     tv.tv_sec = 0;
     tv.tv_usec = 1000 * RETRANS_TIME;
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-
     // Set Address
     memset(&sendaddr, 0, sizeof(sendaddr));
     sendaddr.sin_family = AF_INET;
@@ -56,116 +33,62 @@ int main(int argc, char **argv) {
     AddrInfo recver(sockfd, (struct sockaddr*)&recvaddr, sizeof(recvaddr));
 
     // Create Sender Window
-    window = Window(info.size);
-
+    Window window = Window(info.size);
     // Open Input File and Log File
     std::ifstream ifp(info.iFile);
     std::ofstream log(info.log);
-
     // Set Buffer And Seed
     char buffer[CHUNK_SIZE + 1];
-    int seed = rand();
-
-    // Start Threads
-    std::thread tTimeout(CheckTime, &sender, log);
-    std::thread tAck(CheckAck, &recver, log);
+    unsigned int seed = rand();
 
     // Send Start Packet
     memset(buffer, 0, sizeof(buffer));
     Packet start(START, seed, 0, buffer);
-    SendSignal(start, &sender, log);
-
-    // Reset Timer
-    timer.reset();
+    start.sendPack(&sender, log);
+    while (true) {
+        if (recver.recv(buffer, PACKET_SIZE) <= 0) continue;
+        Packet packet(buffer, log);
+        if (packet.header.type == ACK && packet.header.seqNum == seed) break;
+    }
 
     // Send Packets
     unsigned int seqNum = INIT_SEQ;
-    while (ifp) {
-        // Wait When Window Full
-        std::unique_lock<std::mutex> lck(mtx);
-        while (window.full()) cv.wait(lck);
-        // Read File
+    while (!ifp || !window.empty()) {
+        // Send Packets
+        while (!window.full() && ifp) {
+            // Read File
+            memset(buffer, 0, sizeof(buffer));
+            ifp.read(buffer, CHUNK_SIZE);
+            // Create Packet
+            unsigned int dsize = (unsigned int)ifp.gcount();
+            Packet packet(DATA, seqNum++, dsize, buffer);
+            // Send the packet
+            packet.sendPack(&sender, log);
+            window.push(packet);
+        }
+        // Receive Acks
         memset(buffer, 0, sizeof(buffer));
-        ifp.read(buffer, CHUNK_SIZE);
-        // Create Packet
-        unsigned int dsize = (unsigned int)ifp.gcount();
-        Packet packet(DATA, seqNum++, dsize, buffer);
-        // Send the packet
-        packet.sendPack(&sender, log);
-        lk.lock();
-        window.push(packet);
-        lk.unlock();
-        // Log Sending Activity
-        packet.header.log(log);
+        if (recver.recv(buffer, PACKET_SIZE) > 0) {
+            Packet packet(buffer, log);
+            if (packet.checkSum() && packet.header.type == ACK) {
+                window.cumulForward(packet.header.seqNum);
+            }
+        }
     }
 
     // Send End Packet
     memset(buffer, 0, sizeof(buffer));
     Packet end(END, seed, 0, buffer);
-    SendSignal(end, &sender, log);
+    end.sendPack(&sender, log);
+    while (true) {
+        if (recver.recv(buffer, PACKET_SIZE) <= 0) continue;
+        Packet packet(buffer, log);
+        if (packet.header.type == ACK && packet.header.seqNum == seed) break;
+    }
 
-    // Stop Threads
-    alive = false;
-    tTimeout.join();
-    tAck.join();
-
-    // Close Files
+    // Close Files And Socket
     ifp.close();
     log.close();
+    close(sockfd);
     return 0;
-}
-
-/*  Thread 1:
- *  Check TimeOut. 
- *  When TimeOut, Resend Everything In Window.
- */
-void CheckTime(AddrInfo* sender, std::ofstream& log) {
-    while (alive) {
-        if (timer.exceed()) {
-            // Resend Everything In Window
-            lk.lock();
-            window.sendall(sender, log);
-            lk.unlock();
-            // Reset Timer
-            timer.reset();
-        }
-    }
-}
-
-/*  Thread 2:
- *  Check ACK. 
- *  When Proper ACK Received, Forward (Pop) Window And Notify CV.
- */
-void CheckAck(AddrInfo* recver, std::ofstream& log) {
-    char buffer[PACKET_SIZE + 1];
-    while (alive) {
-        // Listen ACK
-        memset(buffer, 0, sizeof(buffer));
-        if (recver->recv(buffer, PACKET_SIZE) <= 0) continue;
-        // Decode Packet
-        Packet packet(buffer, log);
-        // Checksum
-        if (!packet.checkSum()) continue;
-        // Check Packet Type
-        if (packet.header.type != ACK) continue;
-        // Forward Window
-        lk.lock();
-        window.cumulForward(packet.header.seqNum);
-        lk.unlock();
-        cv.notify_one();
-    }
-}
-
-// Send START And END Message
-void SendSignal(Packet packet, AddrInfo* sender, std::ofstream& log) {
-    // Reset Timer
-    timer.reset();
-    // Send Packet
-    packet.sendPack(sender, log);
-    lk.lock();
-    window.push(packet);
-    lk.unlock();
-    // Block Until Window Empty
-    std::unique_lock<std::mutex> lck(mtx);
-    while (!window.empty()) cv.wait(lck);
 }
