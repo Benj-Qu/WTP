e3d26f76ee07d1d56f4b6fb1df15994d03e4b28d
Benj-Qu <quzhemin@umich.com>
2023-03-11 16:57:34 -0500

    Packet

--- WTP-base/utils/Packet.hpp
+++ WTP-base/utils/Packet.hpp
@@ -13,6 +13,7 @@ public:
     PacketHeader header;
     char data[CHUNK_SIZE + 1];
     bool acked;
+    Packet(unsigned int type, unsigned int seqNum) : header(PacketHeader(type, seqNum, 0, 0));
     Packet(unsigned int type, unsigned int seqNum, unsigned int length, char* _data);
     Packet(char* buffer, std::ofstream& log);
     void sendPack(AddrInfo* sender, std::ofstream& log);
