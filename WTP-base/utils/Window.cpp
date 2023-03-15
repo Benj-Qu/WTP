827c83e05836cea35f30f6d90ff03616e5ab308a
Benj-Qu <quzhemin@umich.com>
2023-03-15 01:19:51 -0400

    Init Receiver Optimization

--- WTP-base/utils/Window.cpp
+++ WTP-base/utils/Window.cpp
@@ -41,9 +41,12 @@ void Window::ack(unsigned int seqNum) {
 }
 
 void Window::receive(Packet packet) {
-    packet.acked = true;
-    if (this->accept(packet.header.seqNum)) {
-        this->packets[packet.header.seqNum - head] = packet;
+    unsigned int index = packet.header.seqNum - head;
+    if (!this->packets[index].acked) {
+        packet.acked = true;
+        if (this->accept(packet.header.seqNum)) {
+            this->packets[packet.header.seqNum - head] = packet;
+        }
     }
 }
 
